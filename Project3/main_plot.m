dcb7036f5978390b1d25110a4ee11ef2a81e77ca
qizhu8 <qizhu8@126.com>
2016-02-16 17:54:47 -0500

    "plot the comparison curves and the union bound"

--- Project3/main_plot.m
+++ Project3/main_plot.m
@@ -1,4 +1,5 @@
 clc,clear,close all
+addpath('./figure_result/')
 load data2_ite1
 ber_a(1,:) = ber;
 save data ber_a;
@@ -15,16 +16,16 @@ load data
 ber_a(3,:) = ber;
 clear ber
 save data
-
 ebn0_range = 10.^(ebn0db / 10.0);
 Pe_bound = 7*qfunc(sqrt(2*ebn0_range*3*4/7)) + 7*qfunc(sqrt(2*ebn0_range*4*4/7)) + qfunc(sqrt(2*ebn0_range*7*4/7));
 
-semilogy(ebn0db, ber_a(1,:), 'r','LineWidth',2); hold on;
-semilogy(ebn0db, ber_a(2,:), 'g','LineWidth',2); hold on;
-semilogy(ebn0db, ber_a(3,:), 'b','LineWidth',2); hold on;
-semilogy(ebn0db, Pe_bound, 'k','LineWidth',2); hold on;
+semilogy(ebn0db, ber_a(1,:), 'r-o','LineWidth',2); hold on;
+semilogy(ebn0db, ber_a(2,:), 'g-x','LineWidth',2); hold on;
+semilogy(ebn0db, ber_a(3,:), 'b-d','LineWidth',2); hold on;
+semilogy(ebn0db, Pe_bound, 'k--','LineWidth',2); hold on;
 axis([-3 6 1e-5 1])
 grid on
 xlabel('E_b/N_0 (dB)','FontSize',16)
 ylabel('BER','FontSize',16)
 legend('iteration = 1', 'iteration = 2', 'iteration = 4', 'union bound');
+delete data.mat
